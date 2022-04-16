dae988c8ad69033f48736468abef79ed703b0c96
vijaya-l <vijaya.ca@outlook.com>
2022-04-16 19:27:58 -0400

    Basic SQL

---
+++ Basic/selectall.sql
@@ -0,0 +1,8 @@
+-- Retrieve everything from a table
+-- Question
+-- How can you retrieve all the information from the cd.facilities table?
+SELECT
+    *
+FROM
+    cd.facilities;
+
---
+++ Basic/selectspecific.sql
@@ -0,0 +1,9 @@
+-- Retrieve specific columns from a table
+-- Question
+-- You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?
+SELECT
+    name,
+    membercost
+FROM
+    cd.facilities;
+
